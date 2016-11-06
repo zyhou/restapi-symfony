9974326546b8c7c018cc6cd5dca5e0cce9d10f44
Maxime <richard.maxime9@gmail.com>
2016-11-06 19:28:18 +0100

    Chagemenr des controller pour utiliser FOSRestBundme

--- src/AppBundle/Controller/PlaceController.php
+++ src/AppBundle/Controller/PlaceController.php
@@ -2,19 +2,18 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Get;
 use AppBundle\Entity\Place;
 
 class PlaceController extends Controller
 {
+
     /**
-     * @Route("/places", name="places_list")
-     * @Method({"GET"})
+     * @Get("/places")
      */
     public function getPlacesAction(Request $request)
     {
@@ -35,14 +34,13 @@ class PlaceController extends Controller
     }
 
     /**
-     * @Route("/places/{place_id}", requirements={"place_id" = "\d+"}, name="places_one")
-     * @Method({"GET"})
+     * @Get("/places/{id}")
      */
-    public function getPlaceAction(Request $request)
+    public function getPlaceAction($id, Request $request)
     {
         $place = $this->getDoctrine()->getManager()
             ->getRepository('AppBundle:Place')
-            ->find($request->get('place_id'));
+            ->find($id);
 
         if (empty($place)) {
             return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,19 +1,17 @@
 <?php
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Get;
 use AppBundle\Entity\User;
 
 class UserController extends Controller
 {
     /**
-     * @Route("/users", name="users_list")
-     * @Method({"GET"})
+     * @Get("/users")
      */
     public function getUsersAction(Request $request)
     {
@@ -35,14 +33,13 @@ class UserController extends Controller
     }
 
     /**
-     * @Route("/users/{id}", requirements={"id" = "\d+"}, name="users_one")
-     * @Method({"GET"})
+     * @Get("/users/{user_id}")
      */
-    public function getUserAction(Request $request)
+    public function getUserAction($user_id, Request $request)
     {
         $user = $this->getDoctrine()->getManager()
             ->getRepository('AppBundle:User')
-            ->find($request->get('id'));
+            ->find($user_id);
 
         if (empty($user)) {
             return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
