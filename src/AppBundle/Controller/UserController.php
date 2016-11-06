82118b70d1c8b84f1f005fa324dad3ef0b57536d
Maxime <richard.maxime9@gmail.com>
2016-11-06 19:05:41 +0100

    Ajout entity et controller User

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,35 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/users", name="users_list")
+     * @Method({"GET"})
+     */
+    public function getUsersAction(Request $request)
+    {
+        $users = $this->getDoctrine()->getManager()
+            ->getRepository('AppBundle:User')
+            ->findAll();
+
+        $formatted = [];
+        foreach ($users as $user) {
+            $formatted[] = [
+                'id' => $user->getId(),
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+                'email' => $user->getEmail(),
+            ];
+        }
+
+        return new JsonResponse($formatted);
+    }
+}
